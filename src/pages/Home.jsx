5c277949175daee5edc057a1ea6663993710de01
a14a19 <anas31197@gmail.com>
2024-01-07 00:54:08 +0530

    careers changed little bit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import HeroSection from "../components/HeroSection"
+import CareersSection from "../components/CareersSection";
 
+// * all the components created should be rendered here
 
 function Home() {
     return (
         <>
             <HeroSection />
+            <CareersSection />
         </>
     )
 }
