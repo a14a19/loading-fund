b40b511e263da014a50e6e0e617a6a40bd3fe504
a14a19 <anas31197@gmail.com>
2024-01-08 23:27:49 +0530

    about file created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection"
 import CareersSection from "../components/CareersSection"
 import Footer from "../components/Footer"
 import Team from "../components/TeamSection"
+import About from "../components/About"
 
 // * all the components created should be rendered here
 
@@ -10,6 +11,7 @@ function Home() {
   return (
     <>
       <HeroSection />
+      <About />
       <Team />
       <CareersSection />
       <Portfolio />
