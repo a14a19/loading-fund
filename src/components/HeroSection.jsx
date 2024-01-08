2b7aa0bd157df70df7e77088c4fd9cfed7d319ee
Ammaar Janjua <aj656@cornell.edu>
2024-01-08 12:31:13 -0500

    first

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -101,35 +101,65 @@ const MovingText = () => {
         Cumque non ullam nemo accusamus dignissimos.</div>
 
 
-      {/* START OF ABOUT SECTION */}
+      {/* start of about section */}
       <div class="left text-wrap " style={{alignSelf:"flex-start"}}>WE’RE CRYPTO-NATIVES WHO HAVE BEEN INVESTING AND BUILDING FOR 10+ YEARS. FOUNDED IN 2018, DRAGONFLY BRINGS ACCESS AND INFLUENCE TO HELP CRYPTO TEAMS WITH GLOBAL ASPIRATIONS FIND INNOVATION AND ADOPTION ANYWHERE.</div>
       <div class="bg-grey"  style={{textAlign:"right"}}>EMBLEM</div>
-      <div class="right text-wrap" style={{textAlign:"right"}}>IN THE SPRING, THE DRAGONFLY IS THE FIRST TO EMERGE FROM THE FROST INTO THE FRESH, GREEN WORLD. IT’S A HUMBLE CREATURE, BUT THE DRAGONFLY HAS ONE OF THE HIGHEST HUNT SUCCESS RATES IN THE ANIMAL KINGDOM.</div>
+      <div class="right text-wrap" style={{textAlign:"right"}}>IN THE SPRING, THE DRAGONFLY IS THE FIRST TO EMERGE FROM THE FROST INTO THE FRESH, GREEN WORLD. IT’S A HUMBLE CREATURE, BUT THE DRAGONFLY HAS ONE OF THE HIGHEST HUNT SUCCESS RATES IN THE ANIMAL KINGDOM.</div><br/>
 
+      {/* image  */}
+      <img src="cube" alt="stats-image"/><br/>
 
-      <div class="orange-banner">
+      {/* stats */}
+      <div class="stats right">
+        <p class="bg-white">STATS</p>
+        <ul>
+          <li class="top border dashed">12        COUNTRIES</li>
+          <li class="top border dashed">48     TEAM MEMBERS</li>
+          <li class="top border dashed">2018   YEAR FOUNDED</li>
+        </ul>
+      </div><br/>
+
+      {/* image  */}
+      <img src="blocks" alt="blocks-image"/><br/>
+
+      {/* orange banner */}
+      <div class="orange-banner left">
+        <img src="triangle" alt="research-logo" />
         <p>DRAGONFLY RESEARCH</p>
         <p>INVESTING IS ALL ABOUT LOOKING INTO THE FUTURE. THROUGH OUR RESEARCH, WE DEVELOP A FUNDAMENTAL UNDERSTANDING OF THE PROBLEMS IN CRYPTO AND WHAT IT TAKES TO MAKE PROGRESS. WE SHARE OUR INSIGHTS INTO DIFFERENT AREAS OF THE CRYPTO WORLD WITH DATA-DRIVEN ANALYSIS AND A TECHNICAL APPROACH TO BREAKING DOWN BIG PROBLEMS AND OPPORTUNITIES.</p>
-      </div>
-      {/* <div>STATS</div>
-      <ul>
-        <li>12     COUNTRIES</li>
-        <li>48     TEAM MEMBERS</li>
-        <li>12     YEAR FOUNDED</li>
-      </ul> */}
-      
-      {/* <div class="md:flex">
-        <div class="md:flex-shrink-0">
-          <img class="rounded-lg md:w-56" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Woman paying for a purchase"/>
-        </div> */}
+      </div><br/>
 
-        {/* <div class="mt-4 md:mt-0 md:ml-6">
-          <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
-            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
-            <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-          </div>
-      </div> */}
-      {/* END OF ABOUT SECTION */}
+      {/* companies */}
+      <ul>
+        <li>ZARTA</li>
+        <li>WOO NETWORK</li>
+        <li>UMA</li>
+        <li>TWALI</li>
+        <li>TRLAB</li>
+        <li>THESIS</li>
+        <li>TAXBIT</li>
+        <li>SZNS</li>
+        <li>SYNFUTURES</li>
+        <li>STARKWARE</li>
+        <li>STABLEHOUSE</li>
+        <li>SPINDL</li>
+        <li>SPICYEST</li>
+        <li>SPACEMESH</li>
+        <li>SOLEND</li>
+        <li>SOCEAN</li>
+        <li>SKOLEM LABS</li>
+        <li>SIPHER</li>
+        <li>SHRAPNEL</li>
+        <li>SHOWTIME</li>
+        <li>SENSE FINANCE</li>
+        <li>SAFFRON FINANCE</li>
+        <li>SADDLE</li>
+      </ul><br/>
+
+      {/* banner */}
+      <span>TECHNOLOGY IS CULTURE    GLOBAL FROM DAY ONE</span><br/>
+
+      {/* end of about section */}
 
 
     </div>
