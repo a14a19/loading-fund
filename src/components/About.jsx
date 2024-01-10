d4ed40fc521f1da115c89d43e00cd5248f01e9f1
Ammaar Janjua <aj656@cornell.edu>
2024-01-10 14:33:39 -0500

    2nd commit to about section, Jan 10

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,91 +1,94 @@
 import React, { useState, useEffect } from 'react';
-
+import cubes from '../assets/cubes.png'
+import blocks from '../assets/blocks.png'
 function About() {
     return (
-     <div id="about">
-        <div className="left text-wrap" style={{alignSelf:"flex-start"}}>WE’RE CRYPTO-NATIVES WHO HAVE BEEN INVESTING AND BUILDING FOR 10+ YEARS. FOUNDED IN 2018, DRAGONFLY BRINGS ACCESS AND INFLUENCE TO HELP CRYPTO TEAMS WITH GLOBAL ASPIRATIONS FIND INNOVATION AND ADOPTION ANYWHERE.</div>
-        <div className="bg-grey"  style={{textAlign:"right"}}>EMBLEM</div>
-        <div className="right text-wrap" style={{textAlign:"right"}}>IN THE SPRING, THE DRAGONFLY IS THE FIRST TO EMERGE FROM THE FROST INTO THE FRESH, GREEN WORLD. IT’S A HUMBLE CREATURE, BUT THE DRAGONFLY HAS ONE OF THE HIGHEST HUNT SUCCESS RATES IN THE ANIMAL KINGDOM.</div><br/>
-        <img src="cube" alt="stats-image"/><br/>
-
+      
+    <div id="about" className="h-auto bg-black px-10"><br /> 
+    {/*  dashed border  */}
+    <div className="border-b border-dashed border-white h-20" /> 
+    {/* heading section */}
+    <section className="flex justify-between gap-4 flex-col md:flex-row text-6xl  lg:text-7xl xl:text-9xl mt-6">
+      <h1 className="text-[#FA4C14]" style={{ fontFamily: "DotGothic16" }}>
+        01
+      </h1>
+      <h1 className="text-white mr-auto md:ml-16 lg:ml-32 font-black">
+        ABOUT<sup>关于</sup>
+      </h1>
+      
+      <div className="w-0 h-0 md:w-12 md:h-12 xl:w-20 xl:h-20">
+        <svg viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg" >
+            <path d="M61 1v60H1V1h60Z" stroke="#FA4C14" strokeMiterlimit="10"></path>
+            <path d="M43.3 38H23.8V18.5" stroke="#FA4C14" strokeMiterlimit="10" ></path>
+            <path d="M23.9 38 60.6 1.3" stroke="#FA4C14"></path>
+        </svg>
+      </div>
+    </section>
+  
       {/* start of about section */}
-      <div className="left text-wrap " style={{alignSelf:"flex-start"}}>WE’RE CRYPTO-NATIVES WHO HAVE BEEN INVESTING AND BUILDING FOR 10+ YEARS. FOUNDED IN 2018, DRAGONFLY BRINGS ACCESS AND INFLUENCE TO HELP CRYPTO TEAMS WITH GLOBAL ASPIRATIONS FIND INNOVATION AND ADOPTION ANYWHERE.</div>
-      <div className="bg-grey"  style={{textAlign:"right"}}>EMBLEM</div>
-      <div className="right text-wrap" style={{textAlign:"right"}}>IN THE SPRING, THE DRAGONFLY IS THE FIRST TO EMERGE FROM THE FROST INTO THE FRESH, GREEN WORLD. IT’S A HUMBLE CREATURE, BUT THE DRAGONFLY HAS ONE OF THE HIGHEST HUNT SUCCESS RATES IN THE ANIMAL KINGDOM.</div><br/>
+      <section className="grid place-items-center sm:place-items-end mt-24">
+        <div className="w-full sm:w-4/5 h-auto border-t border-dashed border-black flex flex-col gap-20 md:gap-0 md:flex-row justify-between pt-10 pb-40 text-balance">
+          <h2 className="text-xl sm:text-2xl md:text-3xl text-white w-full md:w-3/5 md:leading-10 text-balance">
+            <span className="min-md:hidden max-lg:hidden">
+              &emsp;&emsp;&emsp;&emsp;
+            </span>
+            WE’RE CRYPTO-NATIVES WHO HAVE BEEN INVESTING AND BUILDING FOR 10+ YEARS. 
+            FOUNDED IN 2018, DRAGONFLY BRINGS ACCESS AND INFLUENCE TO HELP CRYPTO 
+            TEAMS WITH GLOBAL ASPIRATIONS FIND INNOVATION AND ADOPTION ANYWHERE.
+          </h2>
+        <h2 className="text-md w-full md:w-2/5 text-white md:pl-20 lg:pl-32 text-xs lg:text-sm xl:text-md">
+          <span className="bg-white border-white border text-black p-1 md:p-2 text-[14px]">
+            EMBLEM
+          </span><br /><br />
+          IN THE SPRING, THE DRAGONFLY IS THE FIRST TO EMERGE FROM 
+          THE FROST INTO THE FRESH, GREEN WORLD. IT’S A HUMBLE 
+          CREATURE, BUT THE DRAGONFLY HAS ONE OF THE HIGHEST 
+          HUNT SUCCESS RATES IN THE ANIMAL KINGDOM.
+        </h2>
+        </div>
+      </section>
 
-      {/* image  */}
-      <img src="cube" alt="stats-image"/><br/>
+      {/* cube image */}
+      <img className="border white border-dashed" src={cubes} alt="stats-image"/><br/><br/>
 
       {/* stats */}
-      <div className="stats right">
-        <p className="bg-white">STATS</p>
-        <ul>
-          <li className="top border dashed">12        COUNTRIES</li>
-          <li className="top border dashed">48     TEAM MEMBERS</li>
-          <li className="top border dashed">2018   YEAR FOUNDED</li>
+      <div className="stats left" >
+        <h2 className="text-md w-full md:w-2/5 text-white md:pl-20 lg:pl-32 text-xs lg:text-sm xl:text-md">
+          <span className="bg-white border-white border text-black p-1 md:p-2 text-[14px]">
+            STATS
+          </span><br /><br />
+        </h2>
+
+        <ul className="text-white">
+          <li className="border-t border-dashed border-white h-20">12   <span className=""> COUNTRIES   </span></li>
+          <li className="border-t border-dashed border-white h-20">48   <span className="">  TEAM MEMBERS </span></li>
+          <li className="border-t border-dashed border-white h-20">2018 <span className=""> YEAR FOUNDED</span></li>
         </ul>
       </div><br/>
 
       {/* image  */}
-      <img src="blocks" alt="blocks-image"/><br/>
+      <img src={blocks} alt="blocks-image"/><br/>
 
-      {/* orange banner */}
-      <div className="orange-banner left">
-        <img src="triangle" alt="research-logo" />
-        <p>DRAGONFLY RESEARCH</p>
-        <p>INVESTING IS ALL ABOUT LOOKING INTO THE FUTURE. THROUGH OUR RESEARCH, WE DEVELOP A FUNDAMENTAL UNDERSTANDING OF THE PROBLEMS IN CRYPTO AND WHAT IT TAKES TO MAKE PROGRESS. WE SHARE OUR INSIGHTS INTO DIFFERENT AREAS OF THE CRYPTO WORLD WITH DATA-DRIVEN ANALYSIS AND A TECHNICAL APPROACH TO BREAKING DOWN BIG PROBLEMS AND OPPORTUNITIES.</p>
-      </div><br/>
-
-      {/* companies */}
-    <div className="container"> {/* grid grid-cols-4 gap-4 */}
-    <ul>
-        <div className="row">
-            <div className="text-white col-span-1">
-                <li>ZARTA</li>
-                <li>WOO NETWORK</li>
-                <li>UMA</li>
-                <li>TWALI</li>
-                <li>TRLAB</li>
-                <li>THESIS</li>
-                <li>TAXBIT</li>
-                <li>SZNS</li>
-            </div>
-            <div className="col">
-                <li>SYNFUTURES</li>
-                <li>STARKWARE</li>
-                <li>STABLEHOUSE</li>
-                <li>SPINDL</li>
-                <li>SPICYEST</li>
-                <li>SPACEMESH</li>
-                <li>SOLEND</li>
-                <li>SOCEAN</li>
-            </div><br/>
-            <div className="col">
-                <li>SKOLEM LABS</li>
-                <li>SIPHER</li>
-                <li>SHRAPNEL</li>
-                <li>SHOWTIME</li>
-                <li>SENSE FINANCE</li>
-                <li>SAFFRON FINANCE</li>
-                <li>SADDLE</li>
-            </div><br/>
+      {/* banner: tech is culture */}
+      <div className="texts overflow-hidden w-full bg-black pb-28 text-[#FA4C14] text-4xl lg:text-6xl xl:text-8xl flex flex-row gap-4 font-medium uppercase">
+        <div className="text-slide flex items-center space-x-8 ">
+          <h1>Technology is culture &nbsp;
+            <span style={{ fontFamily: "DotGothic16" }}>
+              global from day one
+            </span>
+          </h1>
         </div>
-    </ul>
-    </div><br/>
-
-      {/* banner */}
-      <span>TECHNOLOGY IS CULTURE    GLOBAL FROM DAY ONE</span><br/>
-
-      {/* end of about section */}
-
-
-
-
+        <div className="text-slide flex items-center space-x-8">
+          <h1>
+            Technology is culture &nbsp;
+            <span style={{ fontFamily: "DotGothic16" }}>
+              global from day one
+            </span>
+          </h1>
+        </div>
+      </div>
       
-     </div>
-
-
-
+    </div>
 
     );
 }
